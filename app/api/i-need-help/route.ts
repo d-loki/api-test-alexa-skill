dea4a1d6d5fb73a021b4223f5bc842721691dc9e
Dimitri <dimitri.guitton.dev@gmail.com>
2024-05-27 17:08:14 +0200

    feat: Envoie le message sur un channel slack de test

--- app/api/i-need-help/route.ts
+++ app/api/i-need-help/route.ts
@@ -1,52 +1,32 @@
 import { WebClient } from '@slack/web-api';
 import { NextResponse } from 'next/server';
 
-export const dynamic = 'force-dynamic'; // defaults to auto
+export const dynamic = 'force-dynamic';
+
 export async function GET( request: Request ) {
     const query = new URL( request.url ).searchParams;
 
-    const toUser   = query.get( 'to' ) || null;
     const fromUser = query.get( 'from' ) || null;
+    const toUser   = query.get( 'to' ) || null;
 
-    const token = process.env.SLACK_TOKEN;
-    const web   = new WebClient( token );
+    const token   = process.env.SLACK_TOKEN;
+    const channel = process.env.SLACK_SEND_TO;
 
-    const users = await web.users.list( {
-                                            limit: 100,
-                                        } );
-
-    let userId;
-    let conversationId;
-
-    if ( users.members ) {
-        const user = users.members.find( user => user.name === process.env.SLACK_SEND_TO );
-        if ( user ) {
-            userId = user.id;
-        }
+    if ( !token || !channel ) {
+        return NextResponse.json( { status: 'KO', error: 'Missing Slack token or channel' }, { status: 500 } );
     }
 
-    if ( userId ) {
-        const conversations = await web.conversations.list( { types: 'im' } );
-        if ( conversations.channels ) {
-            const conversation = conversations.channels.find( conversation => conversation.user === userId );
-            if ( conversation ) {
-                conversationId = conversation.id;
-            }
-        }
+    const web = new WebClient( token );
+
+
+    let prefix = `${ fromUser } demande de l'aide`;
+    if ( !fromUser ) {
+        prefix = 'Une personne dont vous avez la charge demande de l\'aide ';
     }
+    const message = toUser ? `${ prefix } à ${ toUser } via Alexa !` : `${ prefix } via Alexa !`;
+
+    await web.chat.postMessage( { text: message, channel } );
+    return NextResponse.json( { status: 'OK' } );
 
 
-    if ( conversationId ) {
-        let prefix = `${ fromUser } demande de l'aide `;
-        if ( !fromUser ) {
-            prefix = 'Une personne dont vous avez la charge demande de l\'aide ';
-        }
-        const message = toUser ? `${ prefix } à ${ toUser } via Alexa !` : `${ prefix } via Alexa !`;
-
-        await web.chat.postMessage( { text: message, channel: conversationId } );
-        return NextResponse.json( { status: 'OK' } );
-    }
-
-
-    return NextResponse.json( { status: 'KO' } );
 }
