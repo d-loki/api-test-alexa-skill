6792b423c02ba4153397df74340476d23ae40e9f
Dimitri <dimitri.guitton.dev@gmail.com>
2024-05-27 16:41:26 +0200

    refactor: remove la route hello

--- app/api/hello/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export const dynamic = 'force-dynamic'; // defaults to auto
-export async function GET( request: Request ) {
-    const query   = new URL( request.url ).searchParams;
-    const name    = query.get( 'name' ) || 'World';
-    const message = `Hello, ${ name }!`;
-    return NextResponse.json( { message } );
-}
