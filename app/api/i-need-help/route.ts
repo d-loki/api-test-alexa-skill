2d3d8b0de49c0965086cc3c93c35ffdbe092936a
Dimitri <dimitri.guitton.dev@gmail.com>
2024-05-27 16:40:51 +0200

    refactor: refacto du code

--- app/api/i-need-help/route.ts
+++ app/api/i-need-help/route.ts
@@ -3,57 +3,50 @@ import { NextResponse } from 'next/server';
 
 export const dynamic = 'force-dynamic'; // defaults to auto
 export async function GET( request: Request ) {
-    const query  = new URL( request.url ).searchParams;
-    const toUser = query.get( 'to' ) || null;
-    const fromUser = query.get( 'from' ) || 'xxx';
+    const query = new URL( request.url ).searchParams;
+
+    const toUser   = query.get( 'to' ) || null;
+    const fromUser = query.get( 'from' ) || null;
 
     const token = process.env.SLACK_TOKEN;
     const web   = new WebClient( token );
 
-    let userId: string | null         = null;
-    let conversationId: string | null = null;
-    const users                       = await web.users.list( {
-                                                                  limit: 100,
-                                                              } );
+    const users = await web.users.list( {
+                                            limit: 100,
+                                        } );
+
+    let userId;
+    let conversationId;
+
     if ( users.members ) {
-        for ( const user of users.members ) {
-            if ( user.name === process.env.SLACK_SEND_TO ) {
-                userId = user.id ?? null;
-                break;
+        const user = users.members.find( user => user.name === process.env.SLACK_SEND_TO );
+        if ( user ) {
+            userId = user.id;
+        }
+    }
+
+    if ( userId ) {
+        const conversations = await web.conversations.list( { types: 'im' } );
+        if ( conversations.channels ) {
+            const conversation = conversations.channels.find( conversation => conversation.user === userId );
+            if ( conversation ) {
+                conversationId = conversation.id;
             }
         }
     }
 
-    const conversations = await web.conversations.list( {
-                                                            types: 'im',
-                                                        } );
 
-    if ( conversations.channels ) {
-        for ( const conversation of conversations.channels ) {
-            if ( conversation.user === userId ) {
-                conversationId = conversation.id ?? null;
-                break;
-            }
+    if ( conversationId ) {
+        let prefix = `${ fromUser } demande de l'aide `;
+        if ( !fromUser ) {
+            prefix = 'Une personne dont vous avez la charge demande de l\'aide ';
         }
-    }
+        const message = toUser ? `${ prefix } à ${ toUser } via Alexa !` : `${ prefix } via Alexa !`;
 
-    if ( conversationId !== null ) {
-        await ( async () => {
-
-
-            let message = `${ fromUser } a besoin d'aide !`;
-            if ( toUser !== null && toUser !== undefined && toUser !== '' ) {
-                message = `${ fromUser } demande de l'aide à ${ toUser } via Alexa !`;
-            }
-
-            const result = await web.chat.postMessage( {
-                                                           text: message,
-                                                           channel: conversationId,
-                                                       } );
-        } )();
-
+        await web.chat.postMessage( { text: message, channel: conversationId } );
         return NextResponse.json( { status: 'OK' } );
     }
+
 
     return NextResponse.json( { status: 'KO' } );
 }
